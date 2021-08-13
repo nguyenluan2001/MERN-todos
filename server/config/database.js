b627f0283c4e10be9ec5151bf89569d15ef3c074
Nguyen Thanh Luan <ntluan0301@gmail.com>
2021-08-13 16:08:52 +0700

    update code

--- server/config/database.js
+++ server/config/database.js
@@ -3,19 +3,19 @@ async function connect()
 {
     try{
         // ======= CONNECT TO LOCALHOST ==============
-        await mongoose.connect('mongodb://127.0.0.1/mern-todolist', { 
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          });
-          console.log("connect success")
-        // ============ CONNECT TO ATLAS =================
-        // await mongoose.connect('mongodb+srv://luannguyen:ntluan2001@demo-express.6rj7r.mongodb.net/demo-mongo?retryWrites=true&w=majority', {
+        // await mongoose.connect('mongodb://127.0.0.1/mern-todolist', { 
         //     useNewUrlParser: true,
         //     useUnifiedTopology: true,
-        //     useFindAndModify: false,
-        //     useCreateIndex: true
         //   });
         //   console.log("connect success")
+        // ============ CONNECT TO ATLAS =================
+        await mongoose.connect('mongodb+srv://luannguyen:ntluan2001@demo-express.6rj7r.mongodb.net/mern-todos?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+          });
+          console.log("connect success")
     }
     catch(error)
     {
